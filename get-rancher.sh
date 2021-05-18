c728d112ec623c7736092df5691a7201bc11cf20
root <martin.weiss@suse.com>
2021-05-18 11:16:12 +0200

    added rancher 2.5.8, working on rook-ceph

--- get-rancher.sh
+++ get-rancher.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-wget -N https://github.com/rancher/rancher/rancher-2.5.5.tgz
-wget -N https://github.com/rancher/rancher/rancher-2.5.7.tgz
+VERSIONS="2.5.5 2.5.7 2.5.8"
+helm-cli/helm repo add rancher-stable https://releases.rancher.com/server-charts/stable --force-update
+for VERSION in $VERSIONS; do
+#	wget -N https://github.com/rancher/rancher/rancher-$VERSION.tgz -P charts -o rancher-v$VERSION.tgz
+	if [ ! -f helm-cli/helm ]; then
+	      echo "get helm client first to helm-cli/helm"
+	      exit 1
+        fi
+	if [ ! -f charts/rancher-v$VERSION.tgz ] && [ ! -f charts/rancher-$VERSION.tgz ]; then
+		echo "downloading rancher-v$VERSION.tgz"
+		helm-cli/helm fetch rancher-stable/rancher --version $VERSION -d charts
+		mv charts/rancher-$VERSION.tgz charts/rancher-v$VERSION.tgz
+	else 
+		echo "rancher-v$VERSION.tgz already exists"
+	fi
+done
