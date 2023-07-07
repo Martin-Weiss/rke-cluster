87b0e5278eeb1104215427ab472de8a03cc2d928
root <martin.weiss@suse.com>
2023-07-07 14:27:45 +0200

    adding rancher 2.7.5

--- get-rancher.sh
+++ get-rancher.sh
@@ -4,7 +4,8 @@
 #VERSIONS="2.7.1"
 #VERSIONS="2.7.2"
 #VERSIONS="2.7.3"
-VERSIONS="2.7.4"
+#VERSIONS="2.7.4"
+VERSIONS="2.7.5"
 for VERSION in $VERSIONS; do
 #	wget -N https://github.com/rancher/rancher/rancher-$VERSION.tgz -P charts -o rancher-v$VERSION.tgz
 	if [ ! -f helm-cli/helm ]; then
