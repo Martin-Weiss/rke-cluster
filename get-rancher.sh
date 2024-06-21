7cf209974aa56e2ad714c10311437236c5088159
root <martin.weiss@suse.com>
2024-06-21 14:38:25 +0200

    update to rancher 2.8.5

--- get-rancher.sh
+++ get-rancher.sh
@@ -12,7 +12,8 @@
 #VERSIONS="2.8.1"
 #VERSIONS="2.8.2"
 #VERSIONS="2.8.3"
-VERSIONS="2.8.4"
+#VERSIONS="2.8.4"
+VERSIONS="2.8.5"
 
 for VERSION in $VERSIONS; do
 #	wget -N https://github.com/rancher/rancher/rancher-$VERSION.tgz -P charts -o rancher-v$VERSION.tgz
